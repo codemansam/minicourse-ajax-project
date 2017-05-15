a6b5593c2b7be0687c53ca7a0ff35c677d29cd36
codemansam <sam@stacked.co.nz>
2017-05-15 20:08:29 +1200

    NYTimes returns data.  Needs some tweaks though.

--- js/script.js
+++ js/script.js
@@ -27,8 +27,20 @@ function loadData() {
     var NYTimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?=' + cityStr + '&sort=newest&api-key=1d8dfecf2f7c45a5a5855aec78f8d156';
 
        $.getJSON(NYTimesURL, function(data) {
-    	console.log(data);
-    })
+           //console.log(data);
+
+			$nytHeaderElem.text('New York Times Articles About '+ cityStr);
+
+			articles = data.response.docs;
+           
+			for (var i = 0; i < articles.length; i++) {
+				var article = articles[i];
+				$nytElem.append('li class ="article">'+
+				'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+				'<p>' + article.snippet + '</p>' +
+				'</li>');
+			}
+		});
 
     return false;
 };
