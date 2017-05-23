251a3b33efed3748399822935584563ae6116606
codemansam <sam@stacked.co.nz>
2017-05-23 23:29:03 +1200

    Added wikipedia jsonp request.

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ function loadData() {
     var NYTimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?=' + cityStr + '&sort=newest&api-key=1d8dfecf2f7c45a5a5855aec78f8d156';
 
        $.getJSON(NYTimesURL, function(data) {
-           console.log(data);
+           //console.log(data);
 
 			$nytHeaderElem.text('New York Times Articles About '+ cityStr);
 
@@ -35,12 +35,34 @@ function loadData() {
            
 			for (var i = 0; i < articles.length; i++) {
 				var article = articles[i];
-				//$nytElem.append('li class="article">'+
                 $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
 				'<p>' + article.snippet + '</p>' +
 				'</li>');
-			}
-		});
+			};
+            
+		}).error(function(e) {
+            $nytHeaderElem.text('ERROR: AJAX REQUEST FAILED');
+    });
+    // Wikipedia AJAX request goer here
+    var wikiURl = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + cityStr + '&format=json&callback=wikiCallback'; 
+        console.log(wikiUrl);
+
+        $.ajax({
+            url: wikiUrl,
+
+            dataType: "jsonp",
+            // jsonp: "callback",
+            success: function( response ) {
+                var articleList = response[1];
+
+                for (var i = 0; i < articleList.length; i++) {
+                    articleStr = articleList[i];
+                    var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                    $wikiElem.append('<li><a href="' + url + '">' +
+                        articleStr + '</a></li>');
+                }
+            }
+        });    
 
     return false;
 };
